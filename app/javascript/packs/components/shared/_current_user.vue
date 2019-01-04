49de9ba580059c5b90cc6764764398f885f8575f
Junaid Farooq <junaid@evercam.io>
2019-01-04 11:48:54 +0500

    Full name on logout with Gravatar.

--- app/javascript/packs/components/shared/_current_user.vue
+++ app/javascript/packs/components/shared/_current_user.vue
@@ -2,7 +2,7 @@
   <ul class="nav navbar-nav ml-auto">
     <li class="nav-item dropdown">
       <a class="nav-link" data-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
-        <v-gravatar v-bind:email="this.$root.user.email" class="img-avatar" :size="40" />
+        <v-gravatar v-bind:email="this.$root.user.email" class="img-avatar" :size="40" /> <div class="current-user-fullname">{{ this.$root.user.firstname }} {{ this.$root.user.lastname }}</div>
       </a>
       <div class="dropdown-menu dropdown-menu-right">
         <a class="dropdown-item" href="/users/sign_out">
