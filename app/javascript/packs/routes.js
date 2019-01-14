c20971398f464fb59a2333c82da25d75a88a95d8
Junaid Farooq <junaid@evercam.io>
2019-01-14 10:54:52 +0500

    Remove VueBootstrap && configured forced SSL.

--- app/javascript/packs/routes.js
+++ app/javascript/packs/routes.js
@@ -1,11 +1,8 @@
-import Vue from 'vue/dist/vue.esm';
-import VueRouter from 'vue-router';
-import BootstrapVue from 'bootstrap-vue'
-import VueResource from 'vue-resource'
+import Vue from "vue/dist/vue.esm";
+import VueResource from "vue-resource";
 
 Vue.use(VueRouter);
 Vue.use(VueResource)
-Vue.use(BootstrapVue);
 
 import Users from "./components/views/users/users";
 import Page404 from "./components/views/Page404";
