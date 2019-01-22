fc7b68b597db843377a4fd29ffdfdeddc4ffb73a
Junaid Farooq <junaid@evercam.io>
2019-01-22 13:56:19 +0500

    Redirects user if not admin (test.)

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -1,20 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe "Sessions" do
-  it "signs user in and out" do
-    user = User.create(
-      email: 'jonsnow@example.com',
+  setup do
+    @user = User.create(
+      email: "jonsnow@example.com",
       firstname: "jon",
       lastname: "snow",
-      password: 'password123',
-      password_confirmation: 'password123',
+      password: "password123",
+      password_confirmation: "password123",
       is_admin: true
     )
-    sign_in user
+  end
+
+  it "signs user in and out" do
+    sign_in @user
     get root_path
     expect(response).to render_template(:application)
     
-    sign_out user
+    sign_out @user
+    get root_path
+    expect(response).not_to render_template(:application)
+  end
+
+  it "redirects user if not admin" do
+    @user.is_admin = false
+    @user.save
+
+    sign_in @user
     get root_path
     expect(response).not_to render_template(:application)
   end
